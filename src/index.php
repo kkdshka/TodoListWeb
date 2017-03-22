fbb67b5c70516d158b2b2ec742b652a0134582b6
Ksenia Kiselyova <kkdshka@gmail.com>
2017-03-22 17:36:45 +0200

    Refactoring.

--- src/index.php
+++ src/index.php
@@ -6,7 +6,6 @@ use Kkdshka\TodoListWeb\View\TwigRenderer;
 use Kkdshka\TodoList\Model\TaskManager;
 use Kkdshka\TodoList\Repository\RepositoryFactory;
 use Kkdshka\TodoListWeb\Http\Response;
-//use InvalidArgumentException;
 
 $connectionUrl = "csv:C:/Development/Temp/todolist.csv";
 $repository = (new RepositoryFactory)->create($connectionUrl);
@@ -24,39 +23,30 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if ($action == '' && $method == 'GET') {
     $response = $taskController->allAction();
-    respond($response);
 }
 elseif ($action == 'create' && $method == 'GET') {
     $response = $taskController->newAction();
-    respond($response);
 }
 elseif ($action == 'create' && $method == 'POST') {
     $response = $taskController->createAction($_POST);
-    respond($response);
 }
 elseif ($action == 'edit' && $method == 'GET') {
     $response = $taskController->editAction($_GET['id']);
-    respond($response);
 }
 elseif ($action == 'edit' && $method == 'POST') {
     $response = $taskController->updateAction($_GET['id'], $_POST);
-    respond($response);
 }
 elseif ($action == 'complete' && $method == 'GET') {
     $response = $taskController->completeAction($_GET['id']);
-    respond($response);
 }
 elseif ($action == 'delete' && $method == 'GET') {
     $response = $taskController->deleteAction($_GET['id']);
-    respond($response);
 }
 else {
     throw new InvalidArgumentException("Unknown action $action.");
 }
 
-function respond(Response $response) {
-    foreach ($response->getHeaders() as $name => $value) {
-        header("$name: $value");
-    }
-    echo $response->getBody();
+foreach ($response->getHeaders() as $name => $value) {
+    header("$name: $value");
 }
+echo $response->getBody();
